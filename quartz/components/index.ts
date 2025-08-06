06cb5b5af4f2e4e429d05d44607f51247da65dad
catalin-teodor <110928502+catalin-teodor@users.noreply.github.com>
2025-08-06 19:47:43 +0300

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -3,53 +3,63 @@ import TagContent from "./pages/TagContent"
 import FolderContent from "./pages/FolderContent"
 import NotFound from "./pages/404"
 import ArticleTitle from "./ArticleTitle"
-import Darkmode from "./Darkmode"
-import ReaderMode from "./ReaderMode"
-import Head from "./Head"
-import PageTitle from "./PageTitle"
-import ContentMeta from "./ContentMeta"
-import Spacer from "./Spacer"
-import TableOfContents from "./TableOfContents"
-import Explorer from "./Explorer"
-import TagList from "./TagList"
-import Graph from "./Graph"
 import Backlinks from "./Backlinks"
-import Search from "./Search"
-import Footer from "./Footer"
-import DesktopOnly from "./DesktopOnly"
-import MobileOnly from "./MobileOnly"
-import RecentNotes from "./RecentNotes"
+import Body from "./Body"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
-import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import ContentMeta from "./ContentMeta"
 import CustomNav from "./CustomNav"
+import Darkmode from "./Darkmode"
+import Date from "./Date"
+import DesktopOnly from "./DesktopOnly"
+import Explorer from "./Explorer"
+import Flex from "./Flex"
+import Footer from "./Footer"
+import Graph from "./Graph"
+import Head from "./Head"
+import Header from "./Header"
+import MobileOnly from "./MobileOnly"
+import OverflowList from "./OverflowList"
+import PageList from "./PageList"
+import PageTitle from "./PageTitle"
+import ReaderMode from "./ReaderMode"
+import RecentNotes from "./RecentNotes"
+import Search from "./Search"
+import Spacer from "./Spacer"
+import TableOfContents from "./TableOfContents"
+import TagList from "./TagList"
 
 export {
   ArticleTitle,
-  Content,
-  TagContent,
-  FolderContent,
-  Darkmode,
-  ReaderMode,
-  Head,
-  PageTitle,
-  ContentMeta,
-  Spacer,
-  TableOfContents,
-  Explorer,
-  TagList,
-  Graph,
   Backlinks,
-  Search,
-  Footer,
-  DesktopOnly,
-  MobileOnly,
-  RecentNotes,
-  NotFound,
+  Body,
   Breadcrumbs,
   Comments,
-  Flex,
   ConditionalRender,
-  CustomNav
+  Content,
+  ContentMeta,
+  CustomNav,
+  Darkmode,
+  Date,
+  DesktopOnly,
+  Explorer,
+  FolderContent,
+  Footer,
+  Flex,
+  Graph,
+  Head,
+  Header,
+  MobileOnly,
+  NotFound,
+  OverflowList,
+  PageList,
+  PageTitle,
+  ReaderMode,
+  RecentNotes,
+  Search,
+  Spacer,
+  TableOfContents,
+  TagContent,
+  TagList,
 }
