3957b2171e2837ad6b20c3f8cb9b47c301353856
catalin-teodor <110928502+catalin-teodor@users.noreply.github.com>
2025-08-06 13:02:33 +0300

    Update index.ts

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import CustomNav from "./CustomNav"
 
 export {
   ArticleTitle,
@@ -50,6 +51,5 @@ export {
   Comments,
   Flex,
   ConditionalRender,
+  CustomNav
 }
-
-export { default as CustomNav } from "./CustomNav"
