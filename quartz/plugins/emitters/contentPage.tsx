f28f7abf4bb6c5eb7c9d9f073367e2403bf6bc92
catalin-teodor <110928502+catalin-teodor@users.noreply.github.com>
2025-08-06 19:20:03 +0300

    Update contentPage.tsx

--- quartz/plugins/emitters/contentPage.tsx
+++ quartz/plugins/emitters/contentPage.tsx
@@ -50,20 +50,11 @@ export const ContentPage: QuartzEmitterPlugin<Partial<FullPageLayout>> = (userOp
     ...sharedPageComponents,
     ...defaultContentPageLayout,
     pageBody: Content(),
+    afterBody: [],
     ...userOpts,
   }
 
-  const {
-    head: Head,
-    header = [],
-    beforeBody = [],
-    pageBody,
-    afterBody = [],
-    left = [],
-    right = [],
-    footer: Footer,
-  } = opts
-
+  const { head: Head, header, beforeBody, pageBody, afterBody, left, right, footer: Footer } = opts
   const Header = HeaderConstructor()
   const Body = BodyConstructor()
 
@@ -89,10 +80,7 @@ export const ContentPage: QuartzEmitterPlugin<Partial<FullPageLayout>> = (userOp
 
       for (const [tree, file] of content) {
         const slug = file.data.slug!
-        if (slug === "index") {
-          containsIndex = true
-        }
-
+        if (slug === "index") containsIndex = true
         if (slug.endsWith("/index") || slug.startsWith("tags/")) continue
         yield processContent(ctx, tree, file.data, allFiles, opts, resources)
       }
@@ -101,18 +89,17 @@ export const ContentPage: QuartzEmitterPlugin<Partial<FullPageLayout>> = (userOp
         console.log(
           styleText(
             "yellow",
-            `\nWarning: you seem to be missing an \`index.md\` home page file at the root of your \`${ctx.argv.directory}\` folder (\`${path.join(ctx.argv.directory, "index.md")} does not exist\`). This may cause errors when deploying.`,
+            `\nWarning: missing \`index.md\` at the root of \`${ctx.argv.directory}\`. This may cause errors when deploying.`,
           ),
         )
       }
     },
     async *partialEmit(ctx, content, resources, changeEvents) {
       const allFiles = content.map((c) => c[1].data)
-
       const changedSlugs = new Set<string>()
       for (const changeEvent of changeEvents) {
         if (!changeEvent.file) continue
-        if (changeEvent.type === "add" || changeEvent.type === "change") {
+        if (["add", "change"].includes(changeEvent.type)) {
           changedSlugs.add(changeEvent.file.data.slug!)
         }
       }
